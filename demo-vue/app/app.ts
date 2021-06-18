87ae0c348b55ec470ce58dac28c5bd19da3b9666
Jeroen Bobbeldijk <jeroen@klippa.com>
2021-06-18 17:30:44 +0200

    Fix some values

--- demo-vue/app/app.ts
+++ demo-vue/app/app.ts
@@ -7,7 +7,7 @@ declare let __DEV__: boolean;
 Vue.config.silent = !__DEV__
 
 import {wireInGoogleSignIn, wireInFacebookLogin} from "@klippa/nativescript-login";
-wireInGoogleSignIn("833713350466-k2g6vgq8298t4cd9il97qkjnba3qmrpd.apps.googleusercontent.com");
+wireInGoogleSignIn("833713350466-78r5on6lj7ec2ep6k2bjanp8o7391h83.apps.googleusercontent.com");
 wireInFacebookLogin();
 
 new Vue({
